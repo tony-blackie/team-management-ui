0529775ddd059ab04095e3ff564eb4e5c61c7393
Anton Spaskiy <anton.spaskiy@dataart.com>
2016-10-28 17:25:32 +0200

    Add basic dropdown styling Add input for adding new teams

--- src/common/features/team-list/teamListDirective.js
+++ src/common/features/team-list/teamListDirective.js
@@ -7,15 +7,60 @@
         var directive = {
             templateUrl: '../common/features/team-list/team-list.html',
             link: function(scope, element, attrs) {
+
+                window.scope = scope;
+
+                //scope.teamName = "";
+
+                scope.addTeam = function() {
+
+                };
+
+                scope.showDropdown = function(index) {
+                    scope.teams[index].areTeamMembersShown = !scope.teams[index].areTeamMembersShown
+                };
+
                 scope.teams = [
                     {
-                        name: "Team One"
+                        name: "Avengers",
+                        members: [
+                            {
+                                name: 'Bruce Wayne',
+                                job: 'driver'
+                            },
+                            {
+                                name: 'Harley Quinn',
+                                job: 'entertainment manager'
+                            }
+                        ],
+                        areTeamMembersShown: false
                     },
                     {
-                        name: "Avengers"
+                        name: "Big Bang",
+                        members: [
+                            {
+                                name: 'Sheldon Cooper',
+                                job: 'theoretical physisyst'
+                            },
+                            {
+                                name: 'Leonard Hoffstader',
+                                job: 'theoretical physisyst'
+                            },
+                            {
+                                name: 'Radjesh Kootrapali',
+                                job: 'theoretical physisyst'
+                            },
+                            {
+                                name: 'Howard Wolowitz',
+                                job: 'rocket engineer'
+                            }
+                        ],
+                        areTeamMembersShown: false
                     },
                     {
-                        name: "Suicide Squad"
+                        name: "Team One",
+                        members: [],
+                        areTeamMembersShown: false
                     }
                 ];
             },
