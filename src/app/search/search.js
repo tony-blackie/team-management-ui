80853954339af8be0c81a0912a828c06f1ce214a
Anton Spaskiy <anton.spaskiy@dataart.com>
2016-11-03 11:27:37 +0100

    Move team management logic from directive to parent controller

--- src/app/search/search.js
+++ src/app/search/search.js
@@ -1,15 +1,13 @@
 (function() {
     'use strict';
 
-    angular.module('awesome-app.search', ['ui.router']).
-
-    config(function config($stateProvider) {
-
-        $stateProvider
-            .state('search', {
-                url: '^/search',
-                controller: 'SearchCtrl',
-                templateUrl: 'search/search.tpl.html'
-            });
+    angular.module('awesome-app.search', ['ui.router'])
+        .config(function config($stateProvider) {
+            $stateProvider
+                .state('search', {
+                    url: '^/search',
+                    controller: 'TeamListCtrl',
+                    templateUrl: 'search/search.tpl.html'
+                })
     });
 })();
---
+++ src/common/features/team-list/teamListCtrl.js
@@ -0,0 +1,123 @@
+(function() {
+    "use strict";
+
+    angular
+        .module('awesome-app.search')
+        .controller('TeamListCtrl', TeamListCtrl);
+
+    TeamListCtrl.$inject = ['$scope'];
+
+    function TeamListCtrl($scope) {
+        $scope.teams = [
+            {
+                name: "Friends",
+                members: [
+                    {
+                        name: 'Ross Gellar',
+                        job: 'paleonthologist'
+                    },
+                    {
+                        name: 'Chandler Bing',
+                        job: 'accountant'
+                    },
+                    {
+                        name: 'Joey Tribiani',
+                        job: 'actor'
+                    },
+                    {
+                        name: 'Rachel Green',
+                        job: 'fashion designer'
+                    },
+                    {
+                        name: 'Monika Gellar',
+                        job: 'shef'
+                    },
+                    {
+                        name: 'Phoebe Buffay',
+                        job: 'masseuse'
+                    }
+                ],
+                isActive: false
+            },
+            {
+                name: "Big Bang",
+                members: [
+                    {
+                        name: 'Sheldon Cooper',
+                        job: 'theoretical physicist'
+                    },
+                    {
+                        name: 'Leonard Hoffstader',
+                        job: 'theoretical physicist'
+                    },
+                    {
+                        name: 'Radjesh Kootrapali',
+                        job: 'theoretical physicist'
+                    },
+                    {
+                        name: 'Howard Wolowitz',
+                        job: 'rocket engineer'
+                    }
+                ],
+                isActive: false
+            },
+            {
+                name: "True Detective",
+                members: [
+                    {
+                        name: 'Ray Velcoro',
+                        job: 'detective'
+                    },
+                    {
+                        name: 'Frank Semyon',
+                        job: 'businessman'
+                    },
+                    {
+                        name: 'Ani Bezzerides',
+                        job: 'detective'
+                    },
+                    {
+                        name: 'Paul Woodrugh',
+                        job: 'detective'
+                    }
+                ],
+                isActive: false
+            }
+        ];
+
+        function deselectAllOtherTeams(index) {
+            angular.forEach($scope.teams, function(team, i) {
+                if(team.isActive && i !== index) {
+                    $scope.teams[i].isActive = false;
+                }
+            });
+        }
+
+        $scope.addTeam = function() {
+            var stringContainsSpecialCharacters = false;
+            angular.forEach($scope.teamName.split(''), function(char) {
+                if (char.search(/[a-z0-9 ]+/) === -1) {
+                    stringContainsSpecialCharacters = true;
+                }
+            });
+
+            if (!$scope.teamName || $scope.teamName.length < 1 || stringContainsSpecialCharacters) {
+                return;
+            }
+
+            $scope.teams.push(
+                {
+                    name: $scope.teamName,
+                    members: [],
+                    isActive: false
+
+                }
+            );
+        };
+
+        $scope.makeActive = function(index) {
+            $scope.teams[index].isActive = !$scope.teams[index].isActive;
+            deselectAllOtherTeams(index);
+        };
+    }
+})();
